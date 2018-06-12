dd51b061aa202f20a92a20c32989f83b5bc2dea8
dspdevatsina <31406436+dspdevatsina@users.noreply.github.com>
2018-06-12 11:21:53 +0800

    Update WechatSN.java

--- src/com/actions/WechatSN.java
+++ src/com/actions/WechatSN.java
@@ -18,6 +18,7 @@ import com.QRcode;
  *if isprintrepeating == true , it will print out the repeated SN in command window
  *the source file would not include empty line in the whole file.
  *and the structure would be in the same format.
+ *Add Hex Format ,SN start with 50 ,and for dec format ,SN start with 30
  *To deal the task ,modify the parameters of 
  *start 
  *end
@@ -35,8 +36,8 @@ public class WechatSN {
 	public static final String path = "H:\\";
 	public static final String sourfile = "20180517-lost27000-zb-3503_3020040002150451_3020040002200600_50150pcs.txt";
 	public static final String despfile = "result.txt";
-	public static final boolean isprintrepeating = true;     //switch   to print repeat SN in console or not
-	public static final boolean isprintlost      = true;    //switch  to print lost SN in console or not
+	public static final boolean isprintrepeating = true;     //switch   
+	public static final boolean isprintlost      = true;    //switch //still has bug
 	public static final boolean isgenqrcode      = false;    //generate the QR-code picture or not
 	public static void main(String[] args)
 		{
@@ -128,11 +129,52 @@ public class WechatSN {
 		 */
 		public static int findsplitnum(String start)
 		{
-			return 8;
+			if(isHexFormat(start))
+				return 10;
+			else
+				return 8;
 		}
 		public static int totallenthlicense()
 		{
 			return 16;
+		}
+		/*
+		 * is hex format
+		 * start with 50 is hex 
+		 * start with 30 is dec
+		 * */
+		public static boolean isHexFormat(String str)
+		{
+			if(str.startsWith("50"))
+			{
+				return true;
+			}
+			else
+				return false;
+		}
+		/*
+		 * String number to int
+		 * Maybe Hex String to int
+		 * mbhexstrtoint(String , boolean)
+		 * */
+		public static int mbhexstrtoint(String str,boolean ishex)
+		{
+			int a=0;
+			if(ishex)
+			{	
+				try {
+					a = Integer.valueOf(str,16);
+				} catch(NumberFormatException e)
+				{e.printStackTrace();}
+			}
+			else
+			{
+				try {
+					a = Integer.parseInt(str);
+				} catch(NumberFormatException e)
+				{e.printStackTrace();}
+			}
+			return a;
 		}
 		/*
 		 * to correct the format 
@@ -156,51 +198,36 @@ public class WechatSN {
 		{
 			int splitnum = findsplitnum(start);
 			String A = start.substring(splitnum);
-			int a =0;
-			try {
-				a = Integer.parseInt(A);
-			} catch(NumberFormatException e)
-			{e.printStackTrace();}
+			int a =mbhexstrtoint(A,isHexFormat(start));
 			a = a + mid;
 			if(a>0)
-				return correctformat(start.substring(0, splitnum)+ String.valueOf(a));
+			{
+				if(isHexFormat(start))
+				{
+					return correctformat(start.substring(0, splitnum)+ Integer.toHexString(a).toUpperCase());
+				}
+				else
+					return correctformat(start.substring(0, splitnum)+ String.valueOf(a));
+			}
 			else 
 				return "error";
 		}
-		/* int distence(String ,int) 
+		/* int distence(String ,int) -- no more use
 		 * int distance(String, String)
 		 * calculate the total num between start and end
 		 * return positive means a valuable size
 		 * return -1 means error occurs.
 		 * start and end will be split by int splitnum.
 		 */
-		public static int distence(String start, int end)
-		{
-			int splitnum = findsplitnum(start);
-			String A = start.substring(splitnum);
-			int a =0;
-			int b = end;
-			try {
-				a = Integer.parseInt(A);
-			} catch(NumberFormatException e)
-			{e.printStackTrace();}
-			int result = b - a + 1;
-			if(result < 0)
-				result = -1;
-			return result;
-		}
+
 		public static int distence(String start,String end)
 		{
 			int splitnum = findsplitnum(start);
 			String A = start.substring(splitnum);
 			String B = end.substring(splitnum);
-			int a =0;
-			int b =1;
-			try {
-				a = Integer.parseInt(A);
-				b = Integer.parseInt(B);
-			} catch(NumberFormatException e)
-			{e.printStackTrace();}
+			int a =mbhexstrtoint(A,isHexFormat(start));
+			int b =mbhexstrtoint(B,isHexFormat(start));
+
 			int result = b - a + 1;
 			if(result < 0)
 				result = -1;
@@ -248,3 +275,20 @@ public class WechatSN {
 			}catch (IOException ex) {System.out.println(ex.toString());;}		
 		}
 }
+/*
+public static int distence(String start, int end)
+{
+	int splitnum = findsplitnum(start);
+	String A = start.substring(splitnum);
+	int a =0;
+	int b = end;
+	try {
+		a = Integer.parseInt(A);
+	} catch(NumberFormatException e)
+	{e.printStackTrace();}
+	int result = b - a + 1;
+	if(result < 0)
+		result = -1;
+	return result;
+}
+*/
